7a592846da154780aaf27d92726dc366e0982a8f
Tim Isenman <tim.isenman@gmail.com>
2019-09-01 17:33:16 +0900

    Initial Commit

---
+++ YansFirstiOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YansFirstiOSApp
+//
+//  Created by Tim Isenman on 9/1/19.
+//  Copyright © 2019 Shuhari Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ YansFirstiOSAppTests/YansFirstiOSAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  YansFirstiOSAppTests.swift
+//  YansFirstiOSAppTests
+//
+//  Created by Tim Isenman on 9/1/19.
+//  Copyright © 2019 Shuhari Tech. All rights reserved.
+//
+
+import XCTest
+@testable import YansFirstiOSApp
+
+class YansFirstiOSAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
